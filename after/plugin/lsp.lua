25b9e596a2d503534bc426df3b5642305267a04f
Cory Routledge <croutledge@aais.com>
2023-02-08 19:53:43 -0600

    More stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,9 +14,7 @@ require('mason-lspconfig').setup({
   }
 })
 
-
-
-vim.o.completeopt="menuone,noinsert,noselect"
+vim.o.completeopt="menuone,preview,noinsert"
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp_attach = function(client, bufnr)
