136dc7a427bb3261d95d89871f3017675e0bed7d
Cory Routledge <croutledge@aais.com>
2023-02-10 21:44:02 -0600

    Tweaks to lazy git

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,2 +1,10 @@
 ---@diagnostic disable: undefined-global
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { silent=true, desc="Open LazyGit" })
+
+vim.g.lazygit_floating_window_winblend = 1
+vim.g.lazygit_floating_window_scaling_factor = 0.9
+vim.g.lazygit_floating_window_corner_chars = { '╭', '╮', '╰', '╯' }
+vim.g.lazygit_floating_window_use_plenary = 0
+vim.g.lazygit_use_neovim_remote = 1
+vim.g.lazygit_use_custom_config_file_path = 0
+vim.g.lazygit_config_file_path = ''
