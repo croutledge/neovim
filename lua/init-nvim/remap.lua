9b61c0c37d602bd67ada8ea444ee871a5499f3de
Cory Routledge <croutledge@aais.com>
2023-02-12 20:33:26 -0600

    Mappings

--- lua/init-nvim/remap.lua
+++ lua/init-nvim/remap.lua
@@ -1,61 +1,66 @@
 vim.g.mapleader = " "
+
+local keymap = vim.keymap.set
 -- Move a selection up and down (and indent too)
 -- Commented out - these are taken care of by Alt+j/k to avoid
 -- weird typing conflicts
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
 
 -- In normal mode allow moving a line up onto the prior
 -- line but don't send the cursor to the end of the above line
-vim.keymap.set("n", "J", "mzJ`z")
+keymap("n", "J", "mzJ`z")
 
 -- Improve Half page jumping by keeping cursor in center
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
 
 -- When searching keep the search term in the middle
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
 
 -- When you've copied something and then want to
 -- paste over another word, to avoid copying
 -- the word you just pasted over instead it will
 -- retain the original word you had copied
-vim.keymap.set("x", "<leader>p", "\"_dP")
+keymap("x", "<leader>p", "\"_dP")
 
 -- Allow yank into system clipboard with + register
-vim.keymap.set("n", "<leader>y", "\"+y", { silent=true, desc="Copy to System Clipboard" })
-vim.keymap.set("v", "<leader>y", "\"+y", { silent=true, desc="Copy to System Clipboard" })
-vim.keymap.set("n", "<leader>Y", "\"+Y", { silent=true, desc="Copy to System Clipboard" })
+keymap("n", "<leader>y", "\"+y", { silent=true, desc="Copy to System Clipboard" })
+keymap("v", "<leader>y", "\"+y", { silent=true, desc="Copy to System Clipboard" })
+keymap("n", "<leader>Y", "\"+Y", { silent=true, desc="Copy to System Clipboard" })
 
 -- Disable capital Q, its just bad
-vim.keymap.set("n", "Q", "<nop>")
-
--- Quick fix list (via grep) allow for key bindings to nav up and down
--- to what is found instead of just commands
--- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+keymap("n", "Q", "<nop>")
 
 -- Close evrything and exit (with extra umph)
-vim.keymap.set("n", "<leader>qq", ":bufdo q!<CR>", { silent=true, desc="Close all and exit" });
+keymap("n", "<leader>qq", ":bufdo q!<CR>", { silent=true, desc="Close all and exit" });
 
 -- Less keystrokes to navigate splits
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { silent=true, desc="Move to Left Window" });
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { silent=true, desc="Move down a Window" });
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { silent=true, desc="Move to right Window" });
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { silent=true, desc="Move up a Window" });
+keymap("n", "<C-h>", "<C-w><C-h>", { silent=true, desc="Move to Left Window" });
+keymap("n", "<C-j>", "<C-w><C-j>", { silent=true, desc="Move down a Window" });
+keymap("n", "<C-l>", "<C-w><C-l>", { silent=true, desc="Move to right Window" });
+keymap("n", "<C-k>", "<C-w><C-k>", { silent=true, desc="Move up a Window" });
 
--- Location list (via lgrep) is local to the open windows
--- in Vim instead of regular grep which is global
--- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
--- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- Resize buffers easier
+keymap("n", "<C-Up>", ":resize +2<CR>", { silent=true, desc="Horizontal Shrink" })
+keymap("n", "<C-Down>", ":resize -2<CR>", { silent=true, desc="Horizontal Expand" })
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", { silent=true, desc="Vertical Shrink" })
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", { silent=true, desc="Vertical Expand" })
+
+-- Easier escape from insert mode
+keymap("i", "jk", "<ESC>", { silent=true, desc="Exit insert mode" })
+
+-- Easier indents (Shift + < or >)
+keymap("v", "<", "<gv", { silent=true, desc="Exit insert mode" })
+keymap("v", ">", ">gv", { silent=true, desc="Exit insert mode" })
 
 -- Move lines in normal and visual modes with Alt
 -- Visual mode movement allows block moves
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { silent=true, desc="Move Line Down" }) -- move line up(n)
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { silent=true, desc="Move Line Up" }) -- move line down(n)
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent=true, desc="Move Line Down" }) -- move line up(v)
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent=true, desc="Move Line Up" }) -- move line down(v)
+keymap("n", "<A-j>", ":m .+1<CR>==", { silent=true, desc="Move Line Down" }) -- move line up(n)
+keymap("n", "<A-k>", ":m .-2<CR>==", { silent=true, desc="Move Line Up" }) -- move line down(n)
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { silent=true, desc="Move Line Down" }) -- move line up(v)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { silent=true, desc="Move Line Up" }) -- move line down(v)
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
